498933fcbd4d09196e932b9b0c1de63baaf11473
urbsny <16968591+urbsny@users.noreply.github.com>
2021-02-24 19:05:24 +0900

    storybookの追加

---
+++ src/stories/Form.stories.tsx
@@ -0,0 +1,60 @@
+import React, { FC } from 'react';
+
+import { Grid } from '@material-ui/core';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import { Meta, Story } from '@storybook/react';
+import { BaseForm } from 'src/views/base/formik/Form';
+import SubmitButton from 'src/views/base/formik/SubmitButton';
+import { BaseTextField } from 'src/views/base/formik/TextField';
+import { OnSubmit } from 'src/views/base/formik/types';
+import * as yup from 'yup';
+
+type Input = { name?: string };
+
+interface Props {
+  object?: Input;
+  disabled?: boolean;
+  onSubmit?: OnSubmit<Input>;
+}
+
+const Form: FC<Props> = (props) => {
+  const { object, disabled, onSubmit } = props;
+  return (
+    <BaseForm
+      initialValues={object}
+      onSubmit={onSubmit}
+      validationSchema={yup.object({
+        name: yup.string().required().max(10),
+      })}
+    >
+      <CardContent>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={6}>
+            <BaseTextField
+              name="name"
+              label="Name"
+              disabled={disabled}
+              required
+            />
+          </Grid>
+        </Grid>
+      </CardContent>
+      <CardActions>
+        <SubmitButton disabled={disabled} />
+      </CardActions>
+    </BaseForm>
+  );
+};
+
+export default {
+  title: 'Form',
+  component: Form,
+} as Meta;
+
+const Template: Story<Props> = (props) => <Form {...props} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  object: { name: 'abc' },
+};
---
+++ src/stories/utils.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import 'src/base/i18n';
+
+import { Story } from '@storybook/react';
+import { Provider } from 'react-redux';
+import store from 'src/state/store';
+import ThemeProvider from 'src/views/base/material-ui/ThemeProvider';
+
+export const ThemeDecorator = (StoryComponent: Story) => (
+  <ThemeProvider>
+    <StoryComponent />
+  </ThemeProvider>
+);
+export const ReduxDecorator = (StoryComponent: Story) => (
+  <Provider store={store}>
+    <StoryComponent />
+  </Provider>
+);
