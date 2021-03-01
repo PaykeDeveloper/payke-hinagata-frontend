ecbf26ba9af8d81cf59270e7393c2ddfaf3946c1
urbsny <16968591+urbsny@users.noreply.github.com>
2021-03-01 13:55:10 +0900

    リファクタリング

--- src/views/routes/menus.tsx
+++ src/views/routes/menus.tsx
@@ -1,12 +1,18 @@
 import { ListSubheader } from '@material-ui/core';
 import { Trans } from 'react-i18next';
 import {
+  AddIcon,
   HomeIcon,
   ListIcon,
   MenuBookIcon,
 } from 'src/views/base/material-ui/Icon';
 import { MenuList } from 'src/views/components/SideMenu';
-import { booksPath, rootPath } from 'src/views/routes/paths';
+import {
+  bookEditPath,
+  booksNewPath,
+  booksPath,
+  rootPath,
+} from 'src/views/routes/paths';
 
 export const privateMenuLists: MenuList[] = [
   // FIXME: サンプルコードです。
@@ -35,7 +41,13 @@ export const privateMenuLists: MenuList[] = [
             text: <Trans>List</Trans>,
             icon: <ListIcon />,
             to: booksPath,
-            paths: [booksPath],
+            paths: [booksPath, bookEditPath],
+          },
+          {
+            text: <Trans>Add</Trans>,
+            icon: <AddIcon />,
+            to: booksNewPath,
+            paths: [booksNewPath],
           },
         ],
       },
