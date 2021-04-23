c7b312d392cf8f255e110261578a1aaa9332d4d3
ameyamashiro <yamashiro7044@gmail.com>
2021-04-23 18:35:08 +0900

    remove unused

--- src/store/state/domain/sample/divisionProjects/types.ts
+++ src/store/state/domain/sample/divisionProjects/types.ts
@@ -1,51 +1,16 @@
 // FIXME: SAMPLE CODE
 
-export enum FooBarType {
-  Foo = 'foo',
-  Bar = 'bar',
-}
-
 export interface DivisionProject {
   id: string;
-  bookId: number;
-  // confirmed: 0 | 1;
-  // publishDate: string;
-  // approvedAt: string;
-  // amount: number;
-  // column: number;
-  // choices: 'foo' | 'bar';
-  // description: string;
-  // votes: number;
-  // slug: string;
-  // cover: string | null;
+  projectId: number;
 }
 
 export interface DivisionProjectDetail {
   id: string;
-  bookId: number;
-  confirmed: boolean;
-  publishDate: string;
-  approvedAt: string;
-  amount: number;
-  column: number;
-  choices: FooBarType;
-  description: string;
-  votes: number;
-  slug: string;
-  coverUrl: string | null;
+  projectId: number;
 }
 
 export interface DivisionProjectInput {
   id: string;
-  bookId: number;
-  confirmed: boolean;
-  publishDate: string;
-  approvedAt: string;
-  amount: number;
-  column: number;
-  choices: FooBarType;
-  description: string;
-  votes: number;
-  slug: string;
-  cover?: unknown;
+  projectId: number;
 }
