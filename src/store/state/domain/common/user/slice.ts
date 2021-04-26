6667fdc9ec29318dcc99e74b6ad0927d6c92a97b
ameyamashiro <yamashiro7044@gmail.com>
2021-04-26 15:10:11 +0900

    udpate resetAll style

--- src/store/state/domain/common/user/slice.ts
+++ src/store/state/domain/common/user/slice.ts
@@ -1,22 +1,15 @@
 import { authActions } from 'src/store/state/app/auth/slice';
 import { getMyUserApiUrl } from 'src/store/urls';
 import { createEntitySlice, getEntityInitialState } from 'src/store/utils';
-import { getMetaInitialState } from 'src/store/utils/createEntitySlice';
 
 const userSlice = createEntitySlice(
   'user',
   getEntityInitialState(),
   getMyUserApiUrl,
   (state) => state.domain.common.user,
-  {
-    reset: () => getEntityInitialState(),
-  },
-  (builder) => {
-    builder.addCase(authActions.resetAll, (state, _) => {
-      state.entity = undefined;
-      state.meta.fetchEntity = getMetaInitialState();
-    });
-  }
+  undefined,
+  (builder) =>
+    builder.addCase(authActions.resetAll, () => getEntityInitialState())
 );
 
 export const userActions = userSlice.actions;
