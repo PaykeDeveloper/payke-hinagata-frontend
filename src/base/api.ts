9ca9ef097c14a87aeaba98d83949608d68ffea03
S.U <16968591+urbsny@users.noreply.github.com>
2023-02-12 23:11:53 +0900

    環境最新化

--- src/base/api.ts
+++ src/base/api.ts
@@ -2,7 +2,9 @@ import axios, { AxiosError, AxiosHeaders } from 'axios';
 import applyConverters from 'axios-case-converter';
 import { getLanguage } from 'src/base/i18n';
 
-const api = applyConverters(axios.create());
+const api = applyConverters(axios.create(),{
+  caseOptions: { stripRegexp: /[^A-Z0-9[.\]]+/gi },
+});
 api.defaults.withCredentials = true;
 api.interceptors.request.use((config) => {
   let { headers } = config;
